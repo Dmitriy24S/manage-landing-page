2ccd50b65fb2df19c2242ef5f437616fa52f9659
Dmitriy <devilins24@gmail.com>
2022-03-25 20:27:10 +0200

    add slider css, js

--- script.js
+++ script.js
@@ -5,3 +5,38 @@ navToggle.addEventListener("click", () => {
   menu.classList.toggle("show");
   navToggle.classList.toggle("active");
 });
+
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  breakpoints: {
+    // when window width is >= 640px
+    320: {
+      // width: 320,
+      width: 314,
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    // when window width is >= 768px
+    768: {
+      width: 640,
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    // when window width is >= 1078px
+    1078: {
+      width: 950,
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
